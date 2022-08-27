fe84ef69b82f258502dcc9e8ac02e03740ee0cdd
tugkanmeral <tugkan.mrl@gmail.com>
2022-08-27 14:46:41 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import Training1 from './components/Training1.vue'
+</script>
+
+<template>
+    <div class="training-list">
+        <div class="training-container">
+            <h1 class="training-title">reactive & ref</h1>
+            <Training1 />
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.training-list {
+    display: flex;
+    flex-direction: column;
+}
+
+.training-container {
+    display: flex;
+    flex-direction: column;
+    margin: 15px;
+    padding: 10px;
+    border: solid 1px grey;
+    border-radius: 5px;
+}
+
+.training-title {
+    text-decoration: underline;
+}
+</style>
---
+++ src/components/Training1.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+import { reactive, ref } from 'vue'
+
+//reactive() only works on objects (including arrays and built-in types like Map and Set).
+const counter = reactive({
+    count: 0
+})
+const counter2 = reactive({
+    count: 1
+})
+
+// ref() can take any value type and create an object that exposes the inner value under a .value property
+const title = ref('Counter')
+
+// when you want to use a const that described using ref in script block, you must use .value
+console.log(title.value)
+
+</script>
+
+<template>
+    <h4>{{ title }}</h4>
+    <p>{{ counter.count }}</p>
+    <!-- The content inside the mustaches is not limited to just identifiers or paths - we can use any valid JavaScript expression: -->
+    <h4>{{ title.split('').reverse().join('') }}</h4>
+    <p>{{ counter2.count }}</p>
+</template>
